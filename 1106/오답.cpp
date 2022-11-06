7fdb0728efd614067f994a4b7c335e141835493d
SohhnQuixote <ssmin1215@gmail.com>
2022-11-06 23:14:54 +0900

    sohn

--- 1106/오답.cpp
+++ 1106/오답.cpp
@@ -12,35 +12,33 @@ using namespace std;
 
 */
 int c;
-int n,d,p;
+int n, d, p;
 int town[51][51];
 int connected[51];
 double cache[101][51];
 
-double escape(int day,int t)
+double escape(int day, int t)
 {
-  //현재 day에서 출발지 town에서 갈수 있는 모든 경우의 수
-  //이전거에서 현재 확률 곱해서 넣기?
-  if(t ==p)
-    return 0;
-  double &ret = cache[day][t];
-  if(ret) return ret;
-  //연결된 지역으로 역추적
-  ret =0;
-  for(int i=0;i<n;i++)
-    {
-      if(town[i][t])
-      {
-        ret+=(double)(1/(connected[i])) * escape(day-1,i); 
-        
-      }
+	//현재 day에서 출발지 town에서 갈수 있는 모든 경우의 수
+	//이전거에서 현재 확률 곱해서 넣기?
+	double &ret = cache[day][t];
+	if (ret!=-1) return ret;
+	//연결된 지역으로 역추적
+	ret = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (town[i][t])
+		{
+			ret += ((double)(1) / (double)(connected[i])) * escape(day - 1, i);
+
+		}
 
 
-      
-    }
-  return ret;
 
-  
+	}
+	return ret;
+
+
 }
 
 
@@ -48,47 +46,53 @@ double escape(int day,int t)
 
 
 int main() {
-  cin>>c;
-  for(int tc=0;tc<c;tc++)
-    {
-      cin>>n>>d>>p;
-      memset(cache,0,sizeof(cache));
-      memset(connected, 0 ,sizeof(connected));
-      for(int r=0;r<n;r++)
-        {
-          for(int c=0;c<n;c++)
-            {
-              cin>>town[r][c];
-              if(town[r][c])
-                connected[r]++;
-            }
+	cin >> c;
+	for (int tc = 0; tc < c; tc++)
+	{
+		cin >> n >> d >> p;
+		for(int i=0;i<101;i++)
+        for(int j=0;j<51;j++)
+          cache[i][j] = -1;
+    //memset(cache, -1, sizeof(cache));
+		memset(connected, 0, sizeof(connected));
+		for (int r = 0; r < n; r++)
+		{
+			for (int c = 0; c < n; c++)
+			{
+				cin >> town[r][c];
+				if (town[r][c])
+					connected[r]++;
+			}
 
-          
-        }
-        //첫번째 교도소에서 확률 초기화?
-        for(int s=0;s<n;s++)
-          {
-            if(town[p][s])
-            {
-              cache[0][s] = double(1/connected[p]);
-            }
 
-            
-          }
-        for(int s=0;s<n;s++)
-          if(s!=p)
-            escape(d-1,s);
-      int t;
-      cin>>t;
-      for(int s=0;s<t;s++)
-        {
-          int num;
-          cin>>num;
-          printf("%.7f " , cache[d-1][num]);
-          
-        }
-        printf("\n");
+		}
+		//첫번째 교도소에서 확률 초기화?
+		for (int s = 0; s < n; s++)
+		{
+			if (town[p][s])
+			{
+				cache[0][s] = (double)(1) /(double)( connected[p]);
+			}
+      else
+      {
+        cache[0][s] = 0;
+      }
 
-      
-    }
+
+		}
+		for (int s = 0; s < n; s++)
+				escape(d - 1, s);
+		int t;
+		cin >> t;
+		for (int s = 0; s < t; s++)
+		{
+			int num;
+			cin >> num;
+			printf("%.7f ", cache[d - 1][num]);
+
+		}
+		printf("\n");
+
+
+	}
 }
